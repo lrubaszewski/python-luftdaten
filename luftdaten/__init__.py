69c29f4ee0ac6e8e8749eee44102d1b513cf5cd5
rubin <lukasz.rubaszewski@gmail.com>
2020-03-02 01:10:33 +0100

    LufdatenPush added

--- luftdaten/__init__.py
+++ luftdaten/__init__.py
@@ -8,8 +8,110 @@ import async_timeout
 from . import exceptions
 
 _LOGGER = logging.getLogger(__name__)
-_RESOURCE = 'https://api.luftdaten.info/v1'
 
+_RESOURCE = 'https://api.luftdaten.info/v1'
+_PUSH_ENDPOINT = "http://api.luftdaten.info/v1/push-sensor-data/"
+
+SENSOR_SDS011  = 1    # value_types: P1 (PM10), P2 (PM2.5)
+SENSOR_BMP180  = 3    # value_types: temperature, pressure
+SENSOR_BME280  = 11   # value_types: temperature, humidity, pressure
+SENSOR_PMS1003 = 1    # value_types: P1 (PM10), P2 (PM2.5)
+SENSOR_PMS3003 = 1    # value_types: P1 (PM10), P2 (PM2.5)
+SENSOR_PMS5003 = 1    # value_types: P1 (PM10), P2 (PM2.5)
+SENSOR_PMS7003 = 1    # value_types: P0 (PM1), P1 (PM10), P2 (PM2.5)
+
+class LuftdatenPush(object):
+    """A class for handling the data pushing.
+       source:
+         https://github.com/opendata-stuttgart/meta/wiki/APIs#api-luftdateninfo
+    """
+    
+    def __init__(self, sensor_type, chip_id, sw_version, loop, session):
+        """Initialize the connection.
+           sensors:
+                SENSOR_SDS011 
+                SENSOR_BMP180 
+                SENSOR_BME280 
+                SENSOR_PMS1003
+                SENSOR_PMS3003
+                SENSOR_PMS5003
+                SENSOR_PMS7003
+        """
+        self._loop = loop
+        self._session = session
+        self.chip_id = chip_id
+        self.sw_version = sw_version
+        self.data = None
+        self.values = {}
+        self.meta = {}
+        self.url = '{}'.format(_PUSH_ENDPOINT)
+        self.headers = {
+            'content-type': 'application/json',
+            'X-Pin' : '%s' % format(sensor_type),
+            'X-Sensor': self.chip_id
+        }
+
+    """
+    Header:
+        Content-Type: application/json  
+        X-Pin: <n> 
+        X-Sensor: <chip_id, eg. esp8266-12345678>
+    Data:
+
+    SDS011
+        X-Pin: 1  
+        value_types: P1 (PM10), P2 (PM2.5);
+    BMP180:
+        X-Pin: 3
+        value_types: temperature, pressure;
+    BME280:
+        X-Pin: 11
+        value_types: temperature, humidity, pressure; 
+    PMS1003 - PMS 7003:
+        X-Pin: 1
+        value_types: P1 (PM10), P2 (PM2.5);
+
+    """
+    async def push_data(self, data):
+        """Push the data.
+
+           data:
+               Dictionaries of results delivered by sensor
+               format:
+                {
+                    "data_type" : "value", 
+                    "data_type" : "value"
+                }
+               data types:
+               temperature, humidity, pressure, P1, P2
+        """
+        try:
+            sensordatavalues = []
+            for key in data:
+                sensordatavalues.append({"value_type":key,"value":data[key]})
+            payload = {
+                "software_version": self.sw_version, 
+                "sensordatavalues": sensordatavalues
+            }
+            _LOGGER.debug("headers: %s" % str(self.headers))
+            _LOGGER.debug("payload: %s" % str(payload))
+            
+            _LOGGER.debug(
+                "Pushing data to luftdaten.info: %s", self.url)
+            with async_timeout.timeout(60, loop=self._loop):
+                response = await self._session.request('POST', self.url, json=payload, headers=self.headers)
+
+            _LOGGER.debug(
+                "Response from luftdaten.info: %s content_type %s", response.status, response.content_type)
+            if response.content_type == 'text/html':
+                text = await response.text()
+                _LOGGER.debug("Response text: %s", text)
+            elif response.content_type == 'application/json':
+                json = await response.json()
+                _LOGGER.debug(json)
+        except (asyncio.TimeoutError, aiohttp.ClientError):
+            _LOGGER.error("Can not load data from luftdaten.info")
+            raise exceptions.LuftdatenConnectionError()
 
 class Luftdaten(object):
     """A class for handling the data retrieval."""
@@ -23,7 +125,7 @@ class Luftdaten(object):
         self.values = {}
         self.meta = {}
         self.url = '{}/{}'.format(_RESOURCE, 'sensor')
-
+    
     async def get_data(self):
         """Retrieve the data."""
         try:
@@ -34,7 +136,7 @@ class Luftdaten(object):
                 response = await self._session.get(url)
 
             _LOGGER.debug(
-                "Response from luftdaten.info: %s", response.status)
+                "Response from luftdaten.info: %s type %s", response.status, response.content_type)
             self.data = await response.json()
             _LOGGER.debug(self.data)
         except (asyncio.TimeoutError, aiohttp.ClientError):
@@ -68,3 +170,4 @@ class Luftdaten(object):
     async def validate_sensor(self):
         """Return True if the sensor ID is valid."""
         return True if self.data else False
+
